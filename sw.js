8f1eb8d8c932189b36a2711ea705a07f7aad6fd9
seiulipete <peter.linter@gmail.com>
2026-05-03 12:34:39 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,12 @@
 const CACHE='pedrofit-v3';
-const ASSETS=['/','/index.html','/app.js','/manifest.json'];
+const ASSETS=[
+  '/pedro-fitness/',
+  '/pedro-fitness/index.html',
+  '/pedro-fitness/app.js',
+  '/pedro-fitness/manifest.json',
+  '/pedro-fitness/icon-192.png',
+  '/pedro-fitness/icon-512.png',
+];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('/index.html'))));});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('/pedro-fitness/index.html'))));});
